55aff553f528f074aff61eefdf1b614e9bfecb22
shuckle16 <shuckle16@users.noreply.github.com>
2016-03-08 21:19:32 -0500

    Update nber.R

--- nber.R
+++ nber.R
@@ -8,14 +8,28 @@ e <- read_html("http://www.nber.org/papersbyprog/ED_archive.html")
 links <- html_attr(html_nodes(e,"#mainTable a"), "href")
 
 begin <- Sys.time()
-titles <- lapply(links,FUN=function(x){html_text(html_nodes(read_html(x),".title"))})
+titles <- lapply(links,FUN=function(x)
+  {
+    h <- read_html(x)
+    t <- html_text(html_nodes(h,".title"))
+    ps <- html_nodes(h,"p")
+    p <- html_text(ps[grepl(x=html_attr(ps,"style"),pattern="justify")])
+    d <- html_text(html_nodes(h,"p.bibtop"))
+    return(list(t=t,p=p,d=d))
+  })
 Sys.time() - begin
 
-titles <- unlist(titles)
+paras <- unlist(titles)[grepl("p\\b|p1\\b",names(unlist(titles)))]
+dates <- unlist(titles)[which(names(unlist(titles))=="d")]
+titlez <- unlist(titles)[which(names(unlist(titles))=="t")]
+
+df <- data.frame(paras,dates,titlez)
+
+write.csv(x=df,file = "~/nber_education.csv",quote = TRUE)
 
 BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 2))
 
-crp <- Corpus(VectorSource(titles))
+crp <- Corpus(VectorSource(paras))
 
 crp <- tm_map(crp,tolower)
 crp <- tm_map(crp,removePunctuation)
